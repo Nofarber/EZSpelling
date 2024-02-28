1197de62f3faa330d6143dd8e69dc222c993ae8f
Ron <avitalron12@gmail.com>
2024-02-28 19:34:01 +0200

    new jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,25 @@ import theme from './theme'
 import { Routes, Route } from 'react-router-dom'
 import TeacherPage from "./components/TeacherPage";
 import StudentPage from "./components/StudentPage";
-
+import "./App.css";
+import React, { useState } from "react";
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
+import NameForm from "./components/NameForm";
+import MyDocument from "./components/MyDocument";
 
 
 function App() {
-const useUser = useInfo()
-  const setCurrentUser = useUser.SetCurrentUser
-  
+  const [pdfData, setPdfData] = useState(null);
+  const [showPreview, setShowPreview] = useState(false);
+
+  const handleFormSubmit = (data) => {
+    setPdfData(data);
+    setShowPreview(false);
+  };
+
+  const handlePreviewButtonClick = () => {
+    setShowPreview(true);
+  };
   return (
     <>
     <ThemeProvider theme={theme}>
@@ -28,12 +40,31 @@ const useUser = useInfo()
       <Route path="/student_login" element={<Login teacherOrStudent={1}/>}/>
     </Routes> 
     </ThemeProvider>
-{/*     
-      <Login />
-      <Signup />
-      <Authenticate />
-      <button onClick={() => {logout(),setCurrentUser(null)}}>Logout</button>
-      <Chet/> */}
+    <div dir="rtl" style={{margin:"20px"}}>
+      <NameForm onSubmit={handleFormSubmit} />
+      {pdfData && !showPreview && (
+        <button style={{marginTop : "20px"}} onClick={handlePreviewButtonClick}>הצג מסמך</button>
+      )}
+      {showPreview && (
+        <div
+          style={{ border: "1px solid black", margin: "20px", padding: "20px" }}
+        >
+          <h2> PDF תצוגת </h2>
+          <PDFViewer width="500" height="400">
+            <MyDocument formData={pdfData} />
+          </PDFViewer>
+          <p></p>
+          <PDFDownloadLink
+            document={<MyDocument formData={pdfData} />}
+            fileName="מחוייבות_אישית.pdf"
+          >
+            {({ blob, url, loading, error }) =>
+              loading ? "...טוען" : "הורד מסמך"
+            }
+          </PDFDownloadLink>
+        </div>
+      )}
+    </div>
     </>
   );
 }
