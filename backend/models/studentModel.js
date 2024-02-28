5b0779f7d5127a14f1d974b5e6ef6967f9dbddc8
Eden <edenmilo31@gmail.com>
2024-02-28 12:09:16 +0200

    dabase connected

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
+    studentName: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, default: 'student' },
     teacher: { type: Schema.Types.ObjectId, ref: 'Teacher' } 
 });
 
