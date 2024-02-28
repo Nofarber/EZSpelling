74398c97cf23aaa81c10bde69ac96cbb19a7cf5a
Eden <edenmilo31@gmail.com>
2024-02-28 16:02:59 +0200

    e

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const studentController = require('../controller/studentController');
 
 router.post('/studentlog',  studentController.studentLogin);
+router.post('/studentlogout',  studentController.logout);
 
 
 module.exports = router;
